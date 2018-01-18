666d6a1a63852f5e030fba7eb8df8e1315740859
Demo User <demouser@wc129-dhcp-77.brynmawr.edu>
2018-01-18 14:56:01 -0500

    day of the week spinner

--- app/src/main/java/com/example/demouser/bluebus/MainActivity.java
+++ app/src/main/java/com/example/demouser/bluebus/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.demouser.bluebus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner dayoftheWeekSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dayoftheWeekSpinner = findViewById(R.id.daySpinner);
+        ArrayAdapter<CharSequence> days = ArrayAdapter.createFromResource(this, R.array.days_array, android.R.layout.simple_spinner_item);
+        days.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dayoftheWeekSpinner.setAdapter(days);
     }
 }
