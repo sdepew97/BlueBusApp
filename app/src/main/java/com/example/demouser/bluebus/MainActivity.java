fd75744fa4fc1cc744897af23524da4df57d6559
Demo User <demouser@wc129-dhcp-77.brynmawr.edu>
2018-01-19 11:38:35 -0500

    Info stored for the spinner

--- app/src/main/java/com/example/demouser/bluebus/MainActivity.java
+++ app/src/main/java/com/example/demouser/bluebus/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.demouser.bluebus;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ActionMenuView;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.ToggleButton;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     Spinner dayoftheWeekSpinner;
     Button goButton;
     TextView startLabel;
+    String selected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> days = ArrayAdapter.createFromResource(this, R.array.days_array, android.R.layout.simple_spinner_item);
         days.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dayoftheWeekSpinner.setAdapter(days);
+        dayoftheWeekSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selected = parent.getItemAtPosition(position).toString();
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,13 +60,12 @@ public class MainActivity extends AppCompatActivity {
                 gotoSchedule(view);
             }
         });
-
-
     }
 
     public void gotoSchedule(View view){
         Intent intent = new Intent(this, scheduleActivity.class);
         startActivity(intent);
-
     }
+
 }
+
