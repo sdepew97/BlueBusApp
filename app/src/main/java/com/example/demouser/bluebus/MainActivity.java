420eb3384245bde7f3711b235456af9315e50b54
sdepew97 <Supertiger2017>
2018-01-19 11:51:02 -0500

    Dynamic columns

--- app/src/main/java/com/example/demouser/bluebus/MainActivity.java
+++ app/src/main/java/com/example/demouser/bluebus/MainActivity.java
@@ -1,31 +1,25 @@
 package com.example.demouser.bluebus;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ActionMenuView;
-import android.widget.Adapter;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.ToggleButton;
-import android.widget.AdapterView.OnItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
 
     //Get all GUI components
-    public ToggleButton toggleButton;
-    public TimePicker timePicker;
-    public Spinner dayoftheWeekSpinner;
-    public Button goButton;
-    public TextView startLabel;
-    String selected;
+    public static ToggleButton toggleButton;
+    public static TimePicker timePicker;
+    public static Spinner dayoftheWeekSpinner;
+    public static Button goButton;
+    public static TextView startLabel;
 
     public static Boolean location;
     public static int hour;
@@ -49,15 +43,6 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> days = ArrayAdapter.createFromResource(this, R.array.days_array, android.R.layout.simple_spinner_item);
         days.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dayoftheWeekSpinner.setAdapter(days);
-        dayoftheWeekSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selected = parent.getItemAtPosition(position).toString();
-            }
-
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
 
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,28 +58,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //obtain information from GUI
-    public boolean getLoction()
+    public static boolean getLoction()
     {
         return toggleButton.isChecked();
     }
 
-    public int getHour()
+    public static int getHour()
     {
         timePicker.clearFocus();
         Log.d("hour", Integer.toString(timePicker.getCurrentHour()));
         return timePicker.getCurrentHour();
     }
 
-    public int getMinute()
+    public static int getMinute()
     {
         timePicker.clearFocus();
         Log.d("minute", Integer.toString(timePicker.getCurrentMinute()));
         return timePicker.getCurrentMinute();
     }
 
-    public String getAmPm(int hourOfDay)
+    public static String getAmPm(int hourOfDay)
     {
         return (hourOfDay < 12) ? "AM" : "PM";
     }
-}
 
+}
