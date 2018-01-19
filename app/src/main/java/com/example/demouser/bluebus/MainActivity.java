aed548991e662030e15751bbfa40ff54849c05ce
Rose Lin <rlin1@brynmawr.edu>
2018-01-19 11:58:11 -0500

    Spinner info stored

--- app/src/main/java/com/example/demouser/bluebus/MainActivity.java
+++ app/src/main/java/com/example/demouser/bluebus/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     public static int hour;
     public static int minute;
     public static String AM_PM;
+    public static String day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +45,16 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> days = ArrayAdapter.createFromResource(this, R.array.days_array, android.R.layout.simple_spinner_item);
         days.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dayoftheWeekSpinner.setAdapter(days);
+        dayoftheWeekSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                day = parent.getItemAtPosition(position).toString();
+            }
+
+            public void onNothingSelected(AdapterView<?> parent){
+
+        }
+        });
 
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
